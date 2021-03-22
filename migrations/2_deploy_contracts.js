45eac39ffe10d2b41ec649b3e34d6e79b9642336
lixlim <lixuan1406@gmail.com>
2021-03-22 13:41:01 +0800

    deploy config

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,4 +2,10 @@ const Car = artifacts.require("Car");
 const CarNetwork = artifacts.require("CarNetwork");
 
 module.exports = function(deployer){
+  deployer.then(()=>{
+      return deployer.deploy(CarNetwork, "0x3d6Ab2E9C5f38607b57862568bE25e8AEB0C5B5A");
+  }).then((CarNetworkInstance => {
+      console.log(CarNetworkInstance.address);
+      return deployer.deploy(Car, CarNetworkInstance.address);
+  }))
 }
