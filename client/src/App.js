4c252847251fc79daeae99b4cd41f41d25d7149d
zhikuanaw <xiaoichiban@hotmail.com>
2021-03-24 23:55:02 +0800

    add container to main App for better viewing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Login from './features/main/login/login';
-import AddCar from './features/car/add-car/add-car';
+import ViewCarList from './features/car/view-car-list/view-car-list';
+import ViewOneCar from './features/car/view-car-list/view-car';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/add-car" component={AddCar}/>
-          <Route path="/" component={Login}/>
-        </Switch>
-      </BrowserRouter>
+      <div class="container">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/viewCar/:id" component={ViewOneCar} />
+            <Route path="/viewCar" component={ViewCarList}/>
+            <Route path="/" component={Login}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 };
