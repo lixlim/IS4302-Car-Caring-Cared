c60bb436a24f24fb217a54e68aebc65000c7ed7a
Yi Qiong <yiqiong97@gmail.com>
2021-03-25 17:23:42 +0800

    conflict changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route, BrowserRouter, } from "react-router-dom";
 import Login from './features/main/login';
 import Homepage from './features/main/homepage';
-import ViewCar from './features/car/view-car';
 import Testpage from './features/main/testpage';
 import ProtectedRoute from "./features/main/protectedroute";
 import { AuthProvider } from "./features/main/authprovider";
@@ -17,7 +16,6 @@ const App = () => {
         <Route path='/login' component={Login} />
         <ProtectedRoute exact path="/" component={Homepage} />
         <ProtectedRoute exact path="/test" component={Testpage} />
-        <ProtectedRoute exact path="/view-car" component={ViewCar} />
         <ProtectedRoute exact path="/viewCar/:id" component={ViewOneCar} />
         <ProtectedRoute exact path="/viewCar" component={ViewCarList} />
       </AuthProvider>
